6158a3b712f533721eef93f56622b3e80b21dc28
Prerona <145032457+preronagit@users.noreply.github.com>
2025-07-25 21:11:32 +0530

    Update main.py

--- app.py/main.py
+++ app.py/main.py
@@ -4,7 +4,7 @@ from med_api import get_medicine_suggestion
 from interaction_api import check_interactions
 from utils import translate_text
 
-st.set_page_config(page_title="MedSakhi", page_icon="💊")
+st.set_page_config(page_title="MedEasy", page_icon="💊")
 st.title("💊 MedEasy - AI Pharmacist Assistant")
 
 ocr_api_key = st.secrets["ocr_space_api_key"]
